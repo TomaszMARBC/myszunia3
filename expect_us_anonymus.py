4c96ebd485df63ae91b0d5b1c4026e716c0489f8
OPJerry <jeremiasztalik@gmail.com>
2024-03-04 20:55:55 +0100

    testowy_commit

---
+++ expect_us_anonymus.py
@@ -0,0 +1,12 @@
+print('test')
+
+money = 'cash'
+bank = 'your account'
+my_cripto = 'not your bisnes'
+
+while money in bank:
+    transfer money to my_cripto
+
+    if bank == 0
+    print('glhf')
+    break
