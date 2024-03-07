b7d379aa1e862357c8106af0b297742e041b916d
OPJerry <jeremiasztalik@gmail.com>
2024-03-07 19:45:27 +0100

    częściowo udane przyśpieszenie myski w pythonie

---
+++ faster_mouse_test.py
@@ -0,0 +1,19 @@
+import pyautogui
+from time import sleep
+
+str_how_much_faster = input("how many times faster do you want to go?")
+how_much_faster = int(str_how_much_faster)
+while True:
+    x, y =pyautogui.position()
+    sleep(0.001)
+    x1, y1 = pyautogui.position()
+
+    x2 = x1 - x
+    xx = x2 * how_much_faster
+    x3 = x1 + xx
+
+    y2 = y1 - y
+    yy = y2 * how_much_faster
+    y3 = y1 + yy
+
+    pyautogui.moveTo(x3, y3)
