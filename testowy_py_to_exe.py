adbaef9e3ec2c3fb989d6574cea47be106c6455b
Rafał Ścibior <rafal.scibior99@gmail.com>
2024-03-07 17:46:04 +0100

    dodanie kodu do testu dzialania auto_py_to_exe

---
+++ testowy_py_to_exe.py
@@ -0,0 +1,7 @@
+import datetime
+
+aktualny_czas = datetime.datetime.now().strftime("%H:%M:%S")
+
+print("Godzina uruchomienia programu: ", aktualny_czas)
+
+input("Naciśnij enter aby zamknąć okno")
