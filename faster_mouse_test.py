814d182cf0687e149cf388dd32c2da3abda76759
Gal Anonim <metntalisa@gmail.com>
2024-03-23 21:35:06 +0100

    Code review update
    i changed some lines of code to make it clear

--- faster_mouse_test.py
+++ faster_mouse_test.py
@@ -3,27 +3,27 @@ import pyautogui
 import time
 
 def main():
-    str_how_much_faster = 25
+    str_how_much_faster = input('how fast your mouse should move? [1-30] -> ')
     how_much_faster = int(str_how_much_faster)
     start_time = time.time()
     min_movement = 3
-
-    while (time.time() - start_time) < 30:
-        x, y =pyautogui.position()
+    print('After 15 seconds, your setting will went back to defoult')
+    while (time.time() - start_time) < 15:
+        first_position_x , first_position_y =pyautogui.position()
         time.sleep(0.001)
-        x1, y1 = pyautogui.position()
+        second_position_x, second_position_y = pyautogui.position()
 
-        difference_x = x1 - x
-        difference_y = y1 - y
+        difference_x = second_position_x - first_position_x
+        difference_y = second_position_y - first_position_x
 
         if abs(difference_x) >= min_movement or abs(difference_y) >= min_movement:
-            xx = difference_x * how_much_faster
-            x3 = x1 + xx
-            yy = difference_y * how_much_faster
-            y3 = y1 + yy
+            distance_to_move_in_x = difference_x * how_much_faster
+            third_position_x = second_position_x + distance_to_move_in_x
+            distance_to_move_in_y = difference_y * how_much_faster
+            third_position_y = second_position_y + distance_to_move_in_y
 
 
-            pyautogui.moveTo(x3, y3)
+            pyautogui.moveTo(third_position_x, third_position_y)
 
 if __name__=='__main__':
     main()
